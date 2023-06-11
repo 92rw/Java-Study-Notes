449e5f70942707f8b10de514fce55d5611c16be0
92rw <44282768+92rw@users.noreply.github.com>
2023-06-11 16:49:59 +0700

    Update Studynote03_Var.java

--- Studynote03_Var.java
+++ Studynote03_Var.java
@@ -274,3 +274,17 @@ class Alphabet{
 	}
 }	
 	
+//求出1-1/2+1/3-1/4..1/100的和
+class NumberSequence {
+	public static void main(String[] args){
+		double sum = 0.0;
+			for(int i =1;i <= 100 ; i++){
+			if (i % 2 != 0){
+				sum += 1.0/i;//注意此处需为double型，否则会因整数相除无法得出小数
+			}else{
+				sum -= 1.0/i;
+			}
+		}
+		System.out.println("sum = " +sum);
+	}
+}
