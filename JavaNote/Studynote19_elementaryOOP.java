c5d3bf632466e5f710e22e1ed9f94396edaf46d0
92rw <44282768+92rw@users.noreply.github.com>
2023-07-16 03:51:54 +0700

    Update Studynote19_elementaryOOP.java

--- JavaNote/Studynote19_elementaryOOP.java
+++ JavaNote/Studynote19_elementaryOOP.java
@@ -1,4 +1,4 @@
-
+import java.lang.Math;
 
 class beginner{
 	public static void main(String[] args){
@@ -19,13 +19,13 @@ class beginner{
 		if(index != null){
 			if(index != -1)	System.out.println("字符串中存在此元素，其编号为 " + index);
 			else System.out.println("字符串中不存在此元素");
-			
+
 		} else{
 			System.out.println("strs的输入有误，数组不能为null或{}");
 		}
 
 		//第三题相关代码
-		Book book = new Book("古剑奇谭", 200);
+		ThisBook book = new ThisBook("古剑奇谭", 200);
 		book.info();
 		book.updatePrice();//更新价格
 		System.out.println("进行价格更新");
@@ -68,13 +68,13 @@ class A02{
 	}
 }
 
-/*第三题：编写类Book,定义方法updatePrice,实现更改某本书的价格，
+/*第三题：编写类 ThisBook,定义方法updatePrice,实现更改某本书的价格，
 具体：如果价格>150，则更改为150，如果价格>100，更改为100,否则不变；
 */
-class Book{
+class ThisBook{
 	String name;
 	double price;
-	public Book(String name, double price) {
+	public ThisBook(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
@@ -91,7 +91,7 @@ class Book{
 	public void info(){
 		System.out.println("书名=" + this.name +" 价格=" + this.price);
 	}
-} 
+}
 
 //编写类AO4,实现数组的复制功能copyArr,输入旧数组，返回一个新数组，元素和旧数组一样
 class A04{
@@ -108,12 +108,12 @@ class A04{
 
 
 //编程创建一个Calc计算类，在其中定义2个变量表示两个操作数，定义四个方法实现求和、差、乘、商(要求除数为的话，要提示)
-class Calc{
+class ThisCalc{
 	double num1;
 	double num2;
-	public Calc(double num1, double num2){
+	public ThisCalc(double num1, double num2){
 		this.num1 = num1;
-		this num2 = num2;
+		this.num2 = num2;
 	}
 
 	public double sum(){
@@ -153,7 +153,7 @@ class Employee {
 	int age;
 	String job;
 	double sal;
-	
+
 	//因为要求复用构造器，因此先写属性少的
 	//职位，薪水
 	public Employee(String job, double sal){
@@ -171,8 +171,8 @@ class Employee {
 	public Employee(String name, char gender, int age, String job, double sal){
 		this(name, gender, age);//构造器调用只能使用一次
 		this.job = job;
-		this.sal = sal;		
-	}	
+		this.sal = sal;
+	}
 }
 
 /*将对象作为参数传递给方法。
@@ -185,23 +185,23 @@ public void printAreas(Circle c,int times)
 例如，times为5，则输出半径1,2,3,4,5，以及对应的圆面积。
 (4)在main方法中调用printAreas()方法，调用完毕后输出当前半径值。
 */
-import java.lang.Math;
+
 class PassPara{
 	public static void main(String[] args){
-		Circle c = new Circle();
+		ThisCircle c = new ThisCircle();
 		PassObject po = new PassObject();
 		po.printAreas(c, 5);
 	}
 }
 
-class Circle{
+class ThisCircle{
 	double radius;//半径
 	//因为在圆这个类里已经声明了成员变量，类里的方法可以直接调用这个变量，不需要形参
 	//只需要在含参构造器里有就可以了，然后创建对象的时候把参数传给构造器，方法就能不需要参数就能调用了
 	//因为创建的时候有参数的构造器把默认无参构造器覆盖掉了，就没法创建无参对象
 	//写一个无参构造器是因为，有参构造器的创建会覆盖无参构造器，这也算是构造器的重载
-	public Circle(){}
-	public Circle(double radius) {
+	public ThisCircle(){}
+	public ThisCircle(double radius) {
 		this.radius = radius;
 	}
 	//面积
@@ -215,7 +215,7 @@ class Circle{
 }
 
 class PassObject{
-	public void printAreas(Circle c,int times){
+	public void printAreas(ThisCircle c,int times){
 		System.out.println("radius\tarea");
 		for(int i = 1; i <= times; i++){
 			//每次new一个新circle浪费资源，因此复用
