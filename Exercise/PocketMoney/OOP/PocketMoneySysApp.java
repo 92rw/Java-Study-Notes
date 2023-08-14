395bebc6a78b3ed5ff9f13a6948fa86e2309c097
92rw <44282768+92rw@users.noreply.github.com>
2023-08-14 14:49:51 +0700

    Delete Exercise directory

--- Exercise/PocketMoney/OOP/PocketMoneySysApp.java
+++
@@ -1,7 +0,0 @@
-package Exercise.PocketMoney.OOP;
-//实现功能：调用PocketMoneySysOOP中的相关方法，完成功能
-public class PocketMoneySysApp {
-    public static void main(String[] args) {
-        new PocketMoneySysOOP().mainMenu();
-    }
-}
